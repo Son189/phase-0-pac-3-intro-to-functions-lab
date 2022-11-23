938ad393bfc0989cfad34dcaa5237ca8f8f3dedc
son189 <son.august.7334@gmail.com>
2022-11-23 22:41:57 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(toUpperCase)
+}
+function logWhisper(string){
+    console.log(toLowerCase)
+}
+function sayHiToHeadphonesRoommate(string){
+    if (string.toLowerCase()===string){
+    return "I can't hear you!"
+}
+if (string.toUpperCase()===string) {
+    return "YES INDEED!"
+}
+if (string=="let's have dinner together!") {
+return "i would love to!"
+}
+}
